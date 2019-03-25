9f3db8be4b178213032d3edd00800becca9f5d3e
tsareva <e.tsareva@uit.space>
2019-03-25 09:39:20 +0300

    fix notes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, except: %i[index new create]
-  before_action :set_quiz, only: :index
+  before_action :set_quiz, only: %i[index create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new(question_params)
+    question = @quiz.questions.new(question_params)
     if question.save
       redirect_to question
     else
@@ -22,9 +22,8 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    quiz = @question.quiz
     @question.destroy
-    redirect_to quiz_questions_path(quiz)
+    redirect_to quiz_questions_path(@question.quiz)
   end
 
   private
@@ -38,10 +37,10 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:quiz_id, :body)
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
-    render html: '<b>Record was not found!</b>'.html_safe
+    render html: '<b>Record was not found!</b>'.html_safe, status: 404
   end
 end
