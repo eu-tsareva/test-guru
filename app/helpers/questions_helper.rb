91048153a873fa3ffcf6730d8060df367743257e
tsareva <e.tsareva@uit.space>
2019-03-25 14:16:13 +0300

    add footer and helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  TITLES = { 'new' => 'Create new', 'edit' => 'Edit' }.freeze
+
+  def form_header(question)
+    "#{TITLES[action_name]} #{question.quiz.title} question"
+  end
 end
