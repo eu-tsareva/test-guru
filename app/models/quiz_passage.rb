52dd8d0fe5ca888d65616a6a679465836633f5a3
tsareva <e.tsareva@uit.space>
2019-03-29 16:10:21 +0300

    add changes to  quiz passages views

--- app/models/quiz_passage.rb
+++ app/models/quiz_passage.rb
@@ -1,4 +1,6 @@
 class QuizPassage < ApplicationRecord
+  PASSING_SCORE = 85
+
   belongs_to :quiz
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true
@@ -15,6 +17,14 @@ class QuizPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def successful?
+    score >= PASSING_SCORE
+  end
+
+  def score
+    @score ||= (correct_questions * 100.0 / quiz.questions.count).ceil
+  end
+
   private
 
   def before_validation_set_first_question
@@ -26,10 +36,7 @@ class QuizPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    correct_answers_count == (answer_ids.count rescue 0) &&
-      correct_answers_count == correct_answers.where(id: answer_ids).count
+    correct_answers.ids.sort == (answer_ids.map(&:to_i).sort rescue [])
   end
 
   def correct_answers
