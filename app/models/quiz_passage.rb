89830f92cb49de24b16ea31736dcc378b3e9bafc
tsareva <e.tsareva@uit.space>
2019-03-29 15:06:40 +0300

    set next question as model callback in quiz passages

--- app/models/quiz_passage.rb
+++ app/models/quiz_passage.rb
@@ -4,10 +4,10 @@ class QuizPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_question
     save!
   end
 
@@ -19,6 +19,10 @@ class QuizPassage < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = quiz.questions.first if quiz.present?
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
