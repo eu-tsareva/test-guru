f17614488e85a08e5d85f374e57c1b64756ca9ac
tsareva <e.tsareva@uit.space>
2019-04-25 14:59:05 +0300

    remove unused

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,5 @@ module TestGuru
     config.time_zone = 'Moscow'
     config.i18n.available_locales = %i[en ru]
     config.i18n.default_locale = :ru
-
-    config.autoload_paths << "#{Rails.root}/lib/clients"
   end
 end
