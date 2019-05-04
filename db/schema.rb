aa299a7f1b11e0c51353743ae94a7a54d3bc5b49
tsareva <e.tsareva@uit.space>
2019-05-04 09:12:36 +0300

    add index to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_194409) do
+ActiveRecord::Schema.define(version: 2019_05_04_061027) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -25,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_194409) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
   create_table "gists", force: :cascade do |t|
