95a7e44f87090ac8c4890d7a95af9140b5cac873
tsareva <e.tsareva@uit.space>
2019-05-08 15:58:26 +0300

    add badge form without rule, make update and delete work

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,5 +1,46 @@
 class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: %i[edit update destroy]
+
   def index
     @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+    rule = Rule.create(attr: 'test', value: 'test')
+    @badge.rule_id = rule.id
+
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badges_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :image, :rule_id)
   end
 end
